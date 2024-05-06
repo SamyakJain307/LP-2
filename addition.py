44a7a0d4bd01c70b41c917213c41b9bd970cd45a
SamyakJain307 <168991011+SamyakJain307@users.noreply.github.com>
2024-05-06 14:16:57 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -8,7 +8,7 @@ sum = float(num1) + float(num2)
 
 # The below code prints Hello, world!
 
-print('Hello, world!')
+
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
